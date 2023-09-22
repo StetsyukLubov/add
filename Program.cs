12f524fd93a67a653ef30bd6e7e8679c60b52ddd
stetsyuk <stetsyuk@mail.com>
2023-09-22 21:32:11 +0300

    Добавила задание 006

--- Program.cs
+++ Program.cs
@@ -1,14 +1,17 @@
-﻿Console.WriteLine("Введите  имя пользователя");
-string username = Console.ReadLine();
+﻿int a = 1;
+int b = 2;
+int c = 6;
+int d = 8;
+int e = 4;
 
-if (username.ToLower() == "маша")
-{
-    Console.WriteLine( " ура , это же МАША!");
-}
-else
-{
-    Console.Write (username);
-    Console.Write(", привет!");
-        
-    };
+int max = a;
+
+if ( a > max ) max = a;
+if ( b > max ) max = b;
+if ( c > max ) max = c;
+if ( d > max ) max = d;
+if ( e > max ) max = e;
+
+Console.Write ("max= ");
+Console.WriteLine (max);
 
